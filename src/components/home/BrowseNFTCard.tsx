5e82e65cef3f52aae8cbe9c53155822ea73b95d3
Imhandegbelo <abramgeo5@gmail.com>
2025-10-04 02:19:36 +0100

    update: added explore page

--- src/components/home/BrowseNFTCard.tsx
+++ src/components/home/BrowseNFTCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 interface NFTCardProps {
   count: number;
@@ -7,7 +7,9 @@ interface NFTCardProps {
   name: string;
   avatar: string;
   author: string;
+  time?: string;
   isPopular?: boolean;
+  isExplore?: boolean;
 }
 
 export const BrowseNFTCard: React.FC<NFTCardProps> = ({
@@ -18,9 +20,57 @@ export const BrowseNFTCard: React.FC<NFTCardProps> = ({
   author,
   avatar,
   isPopular,
+  isExplore,
+  time,
 }) => {
+  const [timeLeft, setTimeLeft] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState(true);
+
+  // Convert string to seconds
+  const parseTimeToSeconds = useCallback((timeString: string): number => {
+    const [hours, minutes, seconds] = timeString.split(":").map(Number);
+    return hours * 3600 + minutes * 60 + seconds;
+  }, []);
+
+  // Format seconds back to HH:MM:SS
+  const formatTime = (totalSeconds: number): string => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    return [hours, minutes, seconds]
+      .map((unit) => unit.toString().padStart(2, "0"))
+      .join(":");
+  };
+
+  useEffect(() => {
+    // initializes the time
+    setTimeLeft(parseTimeToSeconds(String(time)));
+  }, [time, parseTimeToSeconds]);
+
+  useEffect(() => {
+    if (!isRunning || timeLeft <= 0) return;
+
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime <= 1) {
+          setIsRunning(false);
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [isRunning, timeLeft]);
+
   return (
     <div className="relative rounded-2xl border border-gray-300">
+      {isExplore && (
+        <span className="absolute top-2 left-2 bg-white rounded-full py-2 px-5 font-bold text-sm">
+          {formatTime(timeLeft)}
+        </span>
+      )}
       <span className="absolute top-2 right-2 bg-white rounded-full py-2 px-5 font-bold text-sm">
         {count}
       </span>
@@ -29,26 +79,54 @@ export const BrowseNFTCard: React.FC<NFTCardProps> = ({
         alt={name}
         className="rounded-t-2xl h-[302px] object-cover object-center w-full"
       />
-      <img
-        src={avatar}
-        alt={author}
-        className="border border-white rounded-full size-[3rem] mx-auto -mt-6"
-      />
-      <div className="text-center pb-6 mt-2">
+      {!isExplore && (
+        <img
+          src={avatar}
+          alt={author}
+          className="border border-white rounded-full size-[3rem] mx-auto -mt-6"
+        />
+      )}
+      <div
+        className={`${isExplore ? "text-left px-4" : "text-center"} pb-6 mt-2`}
+      >
         <h3 className="text-lg font-extrabold">{name}</h3>
-        <p className="text-sm text-gray-500">
-          by{" "}
-          <span className={`${isPopular ? "text-primary" : ""}`}>{author}</span>
-        </p>
-        {isPopular ? (
-          <p className="text-center text-gray-600 text-sm mt-4">
-            Lorem ipsum odor amet, consectetuer adipiscing elit.
-          </p>
-        ) : (
-          <div className="flex gap-2 w-fit mx-auto mt-6">
-            <img src="/icons/solana.svg" alt="sol" />
-            <p className="font-bold text-sm">{price} sol</p>
+        {isExplore ? (
+          <div className="space-y-3 mt-4">
+            <div className="flex justify-between items-center">
+              <span className="text-gray-500">Price</span>{" "}
+              <div className="w-fit flex items-center gap-2">
+                <img src="/icons/solana.svg" alt="sol" />
+                <p className="font-bold text-sm">{price} sol</p>
+              </div>
+            </div>
+            <hr className="text-gray-400" />
+            <div className="flex justify-between items-center">
+              <span className="text-gray-500">Author</span>{" "}
+              <div className="w-fit flex items-center gap-2">
+                <img src={avatar} alt={author} />
+                <p className="font-bold text-sm">{author}</p>
+              </div>
+            </div>
           </div>
+        ) : (
+          <>
+            <p className="text-sm text-gray-500">
+              by{" "}
+              <span className={`${isPopular ? "text-primary" : ""}`}>
+                {author}
+              </span>
+            </p>
+            {isPopular ? (
+              <p className="text-center text-gray-600 text-sm mt-4">
+                Lorem ipsum odor amet, consectetuer adipiscing elit.
+              </p>
+            ) : (
+              <div className="flex gap-2 w-fit mx-auto mt-6">
+                <img src="/icons/solana.svg" alt="sol" />
+                <p className="font-bold text-sm">{price} sol</p>
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>
--- src/pages/MarketPlace.tsx
+++ src/pages/MarketPlace.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Navbar } from "../components/layout/Navbar";
 import { Footer } from "../components/shared/Footer";
+import { marketData } from "../data/marketData";
+import { BrowseNFTCard } from "../components/home/BrowseNFTCard";
 
 export const MarketPlace = () => {
   const [filters, setFilters] = useState<
@@ -18,7 +20,7 @@ export const MarketPlace = () => {
       <Navbar logoColor="primary" hasSearch hasShadow />
       <main className="px-6 md:px-16 lg:px-20 py-12">
         <h2 className="text-left">Explore NFTs</h2>
-        <div className="flex justify-between items-center mt-4">
+        <div className="hidden md:flex justify-between mt-4">
           <div className="flex gap-2">
             {[
               "all",
@@ -36,15 +38,20 @@ export const MarketPlace = () => {
             ))}
           </div>
 
-          <div className="mt-12">
-            
-          </div>
-
-          <div className="flex gap-2">
+          <div className="hidden xl:flex gap-2">
             <FilterButton label="Apply filter" onClick={() => ""} />
             <FilterButton label="Sort by" onClick={() => ""} />
           </div>
         </div>
+        <div className="mt-12 grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {marketData.map((data, idx) => (
+            <BrowseNFTCard key={`exp` + idx} {...data} isExplore />
+          ))}
+        </div>
+
+        <button className="mt-12 block pill cursor-pointer mx-auto bg-primary/40 font-semibold text-primary hover:bg-primary hover:text-white w-40 px-8">
+          Load More
+        </button>
       </main>
 
       <Footer />
@@ -65,7 +72,7 @@ const FilterButton: React.FC<ButtonProps> = ({ label, onClick, filter }) => {
         filter === label
           ? "bg-primary/30 text-primary border-primary/30 hover:bg-primary hover:text-white"
           : "border-gray-300 "
-      } text-xs font-bold py-4 px-8 hover:bg-gray-200`}
+      } text-xs font-bold py-2 lg:py-4 px-4 lg:px-8 hover:bg-gray-200`}
     >
       {label === "domain-names" ? "Domain Names" : label}
     </button>
